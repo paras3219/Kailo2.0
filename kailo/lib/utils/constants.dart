bd3632ac5aee84c314c65651ff62c9739fcb5b08
Ankur Attri <ankurattri874@gmail.com>
2021-05-23 11:11:20 +0530

    userSettings

---
+++ kailo/lib/utils/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+InputDecoration ktextFieldDecoration(String hintText) {
+  return InputDecoration(
+      focusedBorder:
+          OutlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          color: Colors.black54,
+        ),
+      ),
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.black54));
+}
+
+TextStyle ktextStyle() {
+  return TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold);
+}
