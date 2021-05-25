f76b271a582e9add82dd76a6bee91c1bc39c55fa
Ankur Attri <ankurattri874@gmail.com>
2021-05-25 11:05:27 +0530

    userSettings&editSttings

--- kailo/lib/Screens/userSettings.dart
+++ kailo/lib/Screens/userSettings.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kailo/Screens/editDetails.dart';
+import 'package:kailo/utils/chart.dart';
 import 'package:kailo/utils/constants.dart';
-
-enum Gender {
-  male,
-  female,
-}
 
 class UserProfile extends StatefulWidget {
   @override
@@ -13,69 +10,125 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
-  TextEditingController dateOfbirthController = new TextEditingController();
-  TextEditingController bioController = new TextEditingController();
-  TextEditingController nameController = new TextEditingController();
-  Gender selectedGenderType;
-  Color selectedGenderButtonColor = Colors.orange;
-  Color unSelectedGenderButtonColor = Colors.transparent;
-  final maxLines = 5;
+  userSettingsOrHealth selectedSettingType = userSettingsOrHealth.profile;
+  PageController _userSettingsPageController = new PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Expanded(
+              child: Container(
+                margin: EdgeInsets.fromLTRB(0, 100, 0, 0),
+                padding: EdgeInsets.all(3.0),
+                height: 60.0,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade400,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _userSettingsPageController.animateToPage(0,
+                              duration: Duration(milliseconds: 250),
+                              curve: Curves.bounceInOut);
+                          selectedSettingType = userSettingsOrHealth.profile;
+                        });
+                      },
+                      child: Expanded(
+                        child: Container(
+                          margin: EdgeInsets.only(right: 14),
+                          width: MediaQuery.of(context).size.width / 2.3,
+                          decoration: BoxDecoration(
+                              color: selectedSettingType ==
+                                      userSettingsOrHealth.profile
+                                  ? Colors.deepPurple.shade800
+                                  : Colors.transparent,
+                              borderRadius: BorderRadius.circular(50.0)),
+                          child: Center(
+                            child: Text(
+                              'Profile',
+                              style: ktextStyle().copyWith(
+                                  color: selectedSettingType ==
+                                          userSettingsOrHealth.profile
+                                      ? Colors.white
+                                      : Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _userSettingsPageController.animateToPage(1,
+                              duration: Duration(milliseconds: 250),
+                              curve: Curves.bounceInOut);
+                          selectedSettingType = userSettingsOrHealth.health;
+                        });
+                      },
+                      child: Expanded(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2.3,
+                          decoration: BoxDecoration(
+                              color: selectedSettingType ==
+                                      userSettingsOrHealth.health
+                                  ? Colors.deepPurple.shade800
+                                  : Colors.transparent,
+                              borderRadius: BorderRadius.circular(50.0)),
+                          child: Center(
+                              child: Text(
+                            'My Health',
+                            style: ktextStyle().copyWith(
+                                color: selectedSettingType ==
+                                        userSettingsOrHealth.health
+                                    ? Colors.white
+                                    : Colors.black),
+                          )),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: PageView(
+              controller: _userSettingsPageController,
+              children: [ViewUserSettings(), MyHealth()],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ViewUserSettings extends StatefulWidget {
+  @override
+  _ViewUserSettingsState createState() => _ViewUserSettingsState();
+}
+
+class _ViewUserSettingsState extends State<ViewUserSettings> {
+  final maxLines = 5;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-                child: Expanded(
-                  child: Container(
-                    padding: EdgeInsets.all(3.0),
-                    height: 60.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(50),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            width: 150.0,
-                            decoration: BoxDecoration(
-                                color: Colors.deepPurple.shade800,
-                                borderRadius: BorderRadius.circular(50.0)),
-                            child: Center(
-                              child: Text(
-                                'Profile',
-                                style:
-                                    ktextStyle().copyWith(color: Colors.white),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            width: 150.0,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(50.0)),
-                            child: Center(
-                                child: Text(
-                              'My Health',
-                              style: ktextStyle(),
-                            )),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
               ),
               SizedBox(height: 40.0),
               Row(
@@ -111,7 +164,8 @@ class _UserProfileState extends State<UserProfile> {
                     fontWeight: FontWeight.w500, color: Colors.black87),
               ),
               TextField(
-                controller: nameController,
+                showCursor: false,
+                readOnly: true,
                 decoration: ktextFieldDecoration("Alice Stark"),
               ),
               SizedBox(height: 30.0),
@@ -121,25 +175,10 @@ class _UserProfileState extends State<UserProfile> {
                     fontWeight: FontWeight.w500, color: Colors.black87),
               ),
               TextFormField(
-                controller: dateOfbirthController,
+                showCursor: false,
+                readOnly: true,
                 decoration: ktextFieldDecoration("11-01-2001")
                     .copyWith(hintText: "Choose a date"),
-                onTap: () async {
-                  DateTime date = DateTime(1900);
-                  FocusScope.of(context).requestFocus(new FocusNode());
-
-                  date = await showDatePicker(
-                      context: context,
-                      initialDate: DateTime.now(),
-                      firstDate: DateTime(1900),
-                      lastDate: DateTime(2100));
-
-                  setState(() {
-                    if (date != null) {
-                      dateOfbirthController.text = date.toString();
-                    }
-                  });
-                },
               ),
               SizedBox(height: 30.0),
               Text(
@@ -153,16 +192,9 @@ class _UserProfileState extends State<UserProfile> {
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
                       child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            selectedGenderType = Gender.male;
-                          });
-                        },
                         child: Container(
                           decoration: BoxDecoration(
-                            color: selectedGenderType == Gender.male
-                                ? selectedGenderButtonColor
-                                : unSelectedGenderButtonColor,
+                            color: Colors.deepOrangeAccent,
                             borderRadius: BorderRadius.circular(10.0),
                             border: Border.all(color: Colors.black45),
                           ),
@@ -188,34 +220,25 @@ class _UserProfileState extends State<UserProfile> {
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            selectedGenderType = Gender.female;
-                          });
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: selectedGenderType == Gender.female
-                                ? selectedGenderButtonColor
-                                : unSelectedGenderButtonColor,
-                            borderRadius: BorderRadius.circular(10.0),
-                            border: Border.all(color: Colors.black45),
-                          ),
-                          height: 60.0,
-                          child: Center(
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(FontAwesomeIcons.female),
-                                Text(
-                                  "Female",
-                                  style: ktextStyle().copyWith(
-                                      fontWeight: FontWeight.w500,
-                                      color: Colors.black87),
-                                ),
-                              ],
-                            ),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(10.0),
+                          border: Border.all(color: Colors.black45),
+                        ),
+                        height: 60.0,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(FontAwesomeIcons.female),
+                              Text(
+                                "Female",
+                                style: ktextStyle().copyWith(
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black87),
+                              ),
+                            ],
                           ),
                         ),
                       ),
@@ -232,40 +255,104 @@ class _UserProfileState extends State<UserProfile> {
               Container(
                 height: maxLines * 24.0,
                 child: TextField(
-                  controller: bioController,
+                  showCursor: false,
+                  readOnly: true,
                   maxLines: maxLines,
                   decoration: ktextFieldDecoration("").copyWith(
-                    hintText: "Enter a message",
                     fillColor: Colors.grey[300],
                     filled: true,
                   ),
                 ),
               ),
-              SizedBox(
-                height: 50.0,
-              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.deepPurple.shade800,
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
-                    height: 50.0,
-                    width: 150.0,
-                    child: Center(
-                      child: Text(
-                        "Save & Continue",
-                        style: ktextStyle().copyWith(color: Colors.white),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EditUserSettings()));
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade800,
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      height: 50.0,
+                      width: 150.0,
+                      child: Center(
+                        child: Text(
+                          "Edit My Profile",
+                          style: ktextStyle().copyWith(color: Colors.white),
+                        ),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 100.0,
+                    height: 200.0,
                   ),
                 ],
               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHealth extends StatefulWidget {
+  @override
+  _MyHealthState createState() => _MyHealthState();
+}
+
+class _MyHealthState extends State<MyHealth> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "Your Health History",
+                  style: ktextStyle().copyWith(fontSize: 20.0),
+                ),
+              ),
+              Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                child: MyHealthChart(),
+              ),
+              ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 5,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+                      height: 100.0,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(5.0),
+                          boxShadow: [
+                            BoxShadow(color: Colors.grey, blurRadius: 3)
+                          ]),
+                          
+                    );
+                  }),
             ],
           ),
         ),
